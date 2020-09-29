836586e9d57205c18b2ae126f81fad9d57243a98
Georgiana Negrea <ngeorgiana@icloud.com>
2020-09-29 21:02:03 +0300

    adding banner component

---
+++ components/Banner.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <b-img src="~/assets/brand-image2.jpg" fluid alt="Responsive image" />
+    <span class="d-flex justify-content-center banner">
+      TRANSPORT GRATUIT LA COMENZI MAI MARI DE 50 LEI
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner'
+}
+</script>
