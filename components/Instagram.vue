a43190de6ad68daf01df4d649f1b6b929a8df0ea
Georgiana Negrea <ngeorgiana@icloud.com>
2020-10-28 17:08:15 +0200

    add instagram component to display ig posts

---
+++ components/Instagram.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <p>Instagram feed</p>
+    <vue-instagram
+      token="myaccesstoken"
+      :count="5"
+    >
+      <template
+        slot="feeds"
+        slot-scope="props"
+      >
+        <li class="fancy-list">
+          {{ props.feed.link }}
+        </li>
+      </template>
+      <template slot="error" slot-scope="props">
+        <div class="fancy-alert">
+          {{ props.error.error_message }}
+        </div>
+      </template>
+    </vue-instagram>
+  </div>
+</template>
+
+<script>
+import VueInstagram from 'vue-instagram'
+
+export default {
+  name: 'InstragramComp',
+  components: {
+    VueInstagram
+  },
+  props: {
+    token: {
+      type: String,
+      require: true,
+      default: 'a'
+    },
+    count: {
+      type: Number,
+      require: true,
+      default: 5
+    }
+  }
+}
+</script>
