a1782d1817ac3078bdbe499a672c5e85e5c43e12
Georgiana Negrea <ngeorgiana@icloud.com>
2020-12-08 22:20:24 +0200

    refactoring code, added deserialize and destructuring

--- store/index.js
+++ store/index.js
@@ -1,17 +1,21 @@
 export const state = () => ({
-  products: []
+  products: [],
+  categories: []
 })
 
 export const mutations = {
   SET_PRODUCTS (state, products) {
     state.products = products
+  },
+  SET_CATEGORIES (state, categories) {
+    state.categories = categories
   }
 }
 export const actions = {
   async loadProducts ({ commit }) {
     try {
-      const res = await this.$axios.get('https://fakestoreapi.com/products')
-      const products = res.data
+      const { data: products } = await getData('https://fakestoreapi.com/products', this.$axios)
+      // const res = await this.$axios.get('https://fakestoreapi.com/products')
       commit('SET_PRODUCTS', products)
     } catch (error) {
       console.error('loadProducts went wrong')
@@ -19,11 +23,48 @@ export const actions = {
   },
   async loadOneProduct ({ commit }, { productId }) {
     try {
-      const res = await this.$axios.get(`https://fakestoreapi.com/products/${productId}`)
-      const product = res.data
+      const { data: product } = await getData(`https://fakestoreapi.com/products/${productId}`, this.$axios)
       commit('SET_PRODUCTS', product)
     } catch (error) {
       console.error('loadOneProduct went wrong')
     }
+  },
+  async loadCategories ({ commit }) {
+    try {
+      const { data: categories } = await getData('https://fakestoreapi.com/products', this.$axios)
+
+      const newArr = deserializeCategories(categories)
+
+      commit('SET_CATEGORIES', newArr)
+    } catch (error) {
+      console.error('loadCategories went wrong')
+    }
+  }
+  // async loadProductsByCategory ({ commit }, { category }) {
+  //   try {
+  //     const res = await this.$axios.get(`https://fakestoreapi.com/products/category/${category}`)
+  //     const products = res.data
+  //     console.log(products)
+  //     commit('SET_PRODUCTS', products)
+  //   } catch (error) {
+  //     console.error('loadProductsByCategory went wrong')
+  //   }
+  // }
+}
+
+const deserializeCategories = function (categories) {
+  const seen = new Set()
+  const newArr = categories.filter((el) => {
+    const duplicate = seen.has(el.category)
+    seen.add(el.category)
+    return !duplicate
+  })
+  return newArr
+}
+
+const getData = async function (url, axios) {
+  const response = await axios.get(url)
+  return {
+    data: response.data
   }
 }
